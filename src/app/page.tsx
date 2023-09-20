906fdc464a1ab27da734f0ca1391b34b4503071e
rodrigo <rodrigowelme.77@hotmail.com>
2023-09-19 21:02:44 -0300

    portfolio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
       </div>
 
       <div className="flex container mx-auto items-center py-16">
-        <div className="flex flex-wrap gap-2 justify-items-center max-w-fit">
+        <div className="flex flex-1 gap-2 justify-items-center">
           <div className="bg-slate-600 w-44 h-44"></div>
           <div className="bg-slate-600 w-44 h-44"></div>
           <div className="bg-slate-600 w-44 h-44"></div>
@@ -32,16 +32,20 @@ export default function Home() {
           <div className="bg-slate-600 w-44 h-44"></div>
         </div>
         <div>
-          <div>
-            <ul className="steps steps-vertical">
-              <li data-content="" className="step step-primary after:!w-5 after:!h-5"></li>
-              <li data-content="" className="step after:!w-5 after:!h-5 before:!w-2"></li>
-              <li data-content="" className="step after:!w-5 after:!h-5 before:!w-2"></li>
-            </ul>
-          </div>
+          <ul className="steps steps-vertical">
+            <li data-content="" className="step step-primary after:!w-5 after:!h-5"></li>
+            <li data-content="" className="step after:!w-5 after:!h-5 before:!w-2"></li>
+            <li data-content="" className="step after:!w-5 after:!h-5 before:!w-2"></li>
+          </ul>
         </div>
       </div>
     </main>
   );
 }
+
+
+
+
+
+
 
